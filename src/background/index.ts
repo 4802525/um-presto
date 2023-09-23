a6e7cea155bd566c4e369ca83f48c60ee10bc852
4802525 <afxa5558@gmail.com>
2023-09-23 10:40:54 +0900

    不要なページを削除

--- src/background/index.ts
+++ src/background/index.ts
@@ -8,12 +8,3 @@ store.subscribe(() => {
   // const state = store.getState();
   // console.log('state', state);
 });
-
-// show welcome page on new install
-browser.runtime.onInstalled.addListener(async (details) => {
-  if (details.reason === 'install') {
-    //show the welcome page
-    const url = browser.runtime.getURL('welcome/welcome.html');
-    await browser.tabs.create({ url });
-  }
-});

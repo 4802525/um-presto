d02dafb8b352dd4c74a9fec75935bc83a39afec9
4802525 <afxa5558@gmail.com>
2023-09-23 10:32:13 +0900

    edit project

--- src/manifest.ts
+++ src/manifest.ts
@@ -7,8 +7,8 @@ import { version } from '../package.json';
 
 const manifest = defineManifest(async (env) => ({
   manifest_version: 3,
-  name: `${env.mode === 'development' ? '[Dev] ' : ''}Browser Extension TypeScript & React Starter`,
-  description: 'Browser Extension, TypeScript, React',
+  name: `${env.mode === 'development' ? '[Dev] ' : ''}Um Salesforce Vivache`,
+  description: 'Um Salesforce Vivache',
   version,
   background: {
     service_worker: 'background/index.ts',
